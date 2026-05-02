ff89e092260bdef3cb8f0b926bf1f1eb3460ecc3
Beyza Türk <beyzaturk@Beyza-MacBook-Pro.local>
2026-05-02 19:33:35 +0300

    project init

---
+++ helloAzure/helloAzure/Program.cs
@@ -0,0 +1,48 @@
+using Npgsql;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+var keyVaultUrl = "https://kv-final2.vault.azure.net/";
+
+var secretClient = new SecretClient(
+    new Uri(keyVaultUrl),
+    new DefaultAzureCredential()
+);
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseHttpsRedirection();
+
+app.MapGet("/hello", async () =>
+{
+    try
+    {
+        
+        var dbUrl = (await secretClient.GetSecretAsync("db-url")).Value.Value;
+        var dbUser = (await secretClient.GetSecretAsync("db-username")).Value.Value;
+        var dbPassword = (await secretClient.GetSecretAsync("db-password")).Value.Value;
+
+        var connectionString =
+            $"Server={dbUrl};Database=postgres;Port=5432;User Id={dbUser};Password={dbPassword};Ssl Mode=Require;";
+
+        await using var conn = new NpgsqlConnection(connectionString);
+        await conn.OpenAsync();
+
+        return Results.Ok("Database connection SUCCESSFUL");
+    }
+    catch (Exception ex)
+    {
+        return Results.Problem($"Database connection FAILED: {ex.Message}");
+    }
+});
+
+app.Run();
